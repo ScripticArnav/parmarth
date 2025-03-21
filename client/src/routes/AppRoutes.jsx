a0d4aec12512c76138193c08bb0587e1fe54ad7b
ScripticArnav <arnav.1807prof@gmail.com>
2025-03-22 02:21:25 +0530

    new images and team section commit

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -12,6 +12,7 @@ import AuthContext from "../store/auth-context";
 import AddRteData from "../pages/AddRteData/AddRteData";
 import RteData from "../pages/RteData/RteData";
 import Volunteers from "../pages/VolunteersData/Volunteers";
+import Team from "../pages/Team/Team"
 import CreatePost from "../pages/CreatePost/CreatePost";
 import EditPost from "../pages/EditPost/EditPost";
 import ListPost from "../pages/ListPosts/ListPosts";
@@ -58,6 +59,7 @@ const AppRoutes = () => {
         <Route path="/verify-code" element={<VerifyCode />} />
       )}
       <Route path="/about" element={<About />} />
+      <Route path="/team" element={<Team />} />
       <Route path="/classes" element={<Classes />} />
       <Route path="/classes" element={<Classes />} />
       <Route path="/schooling" element={<Schooling />} />
